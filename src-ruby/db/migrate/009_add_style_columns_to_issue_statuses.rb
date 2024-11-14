92a72dbc88317e8be137a2480e5780ed00e53080
Jak <jak@giveon.me>
2024-11-14 16:43:47 -0500

    Added Style Columns to issue statuses

---
+++ src-ruby/db/migrate/009_add_style_columns_to_issue_statuses.rb
@@ -0,0 +1,13 @@
+class AddMondayColumnsToUsers < ActiveRecord::Migration[4.2]
+  def self.up
+    add_column :issue_statuses, :icon, :string, null: true
+    add_column :issue_statuses, :text_color, :string, null: true
+    add_column :issue_statuses, :background_color, :string, null: true
+  end
+
+  def self.down
+    remove_column :issue_statuses, :icon
+    remove_column :text_color, :icon
+    remove_column :background_color, :icon
+  end
+end
